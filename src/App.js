de56090a526cc996d643921a27b2c8ded4c7c3da
Lowin <lowin98@live.it>
2022-09-10 14:58:54 +0200

    Added type,price, status, km and more subcategories

--- src/App.js
+++ src/App.js
@@ -16,13 +16,16 @@ function App() {
 
   const Products = 
     [
-       <Product type= "Sport" name ="2021 Lamborghini Huracan EVO" img="./img/LamborghiniHuracanEVO.jpg" price="333633" id="0" />,
-       <Product type= "Sport" name ="2021 Mercedes-Benz AMG GT" img="./img/2021 Mercedes-Benz AMG GT.jpeg" price= " 131750" id="1"/>,
+       <Product type= "Sport" name ="Lamborghini Huracan EVO" img="./img/LamborghiniHuracanEVO.jpg" price="333633" year="2021" status="new"  km ="0" id="0" />,
+       <Product type= "Sport" name ="Mercedes-Benz AMG GT" img="./img/2021 Mercedes-Benz AMG GT.jpeg" price= " 131750" year="2021" status="used" km="22345" id="1"/>,
      
-       <Product type= "Coupe" name ="ASTON MARTIN DB9 Coupé 6.0 V12 " img="./img/ASTON MARTIN DB9 Coupé 6.0 V12 (Coupé).jpg" price= "39.950" id="2"/>,
-       <Product type= "Coupe" name ="TESLA Model 3 Long R. Dual AWD (Limousine) " img="./img/TESLA Model 3 Long R. Dual AWD (Limousine)(coupe).jpg" price= "47400" id="3"/>,
-       <Product type= "Suv" name ="2021 CHEVROLET trailblazer 4dr suv " img="./img/2021_chevrolet_trailblazer_4dr-suv.jpg" price= "47400" id="4"/>,
-       <Product type= "Suv" name ="2022 VOLKSWAGEN taos 4dr suv " img="./img/2022_volkswagen_taos_4dr-suv.jpg" price= "47400" id="5"/>,
+       <Product type= "Coupe" name ="ASTON MARTIN DB9 Coupé 6.0 V12 " img="./img/ASTON MARTIN DB9 Coupé 6.0 V12 (Coupé).jpg" price= "39950" status="used" km="43000" id="2"/>,
+       <Product type= "Coupe" name ="TESLA Model 3 Long R. Dual AWD" img="./img/TESLA Model 3 Long R. Dual AWD (Limousine)(coupe).jpg" price= "47400" status="new" km="0" id="3"/>,
+       <Product type= "Suv" name ="CHEVROLET trailblazer 4dr" img="./img/2021_chevrolet_trailblazer_4dr-suv.jpg" price= "37600" year="2021" status="used" km="85432" id="4"/>,
+       <Product type= "Suv" name ="VOLKSWAGEN taos 4dr" img="./img/2022_volkswagen_taos_4dr-suv.jpg" price= "66400" year="2022" status="used" km="12300" id="5"/>,
+       <Product type= "Hatchback" name ="CHEVROLET bolt ev" img="./img/2022-chevrolet-bolt-ev-(hatchbacks).jpg" price= "126400" year="2022" status="new" km="0" id="6"/>,
+       <Product type= "Hatchback" name ="KIA rio" img="./img/Kia rio (hatchbacks).jpg" price= "136800" year="2022" status="new" km="0" id="7"/>,
+       
        
     ]
 
--- src/component/ShoppingPage.js
+++ src/component/ShoppingPage.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Link, useOutletContext } from "react-router-dom";
-
+import Form from 'react-bootstrap/Form';
 import { ShoppingCart } from "./ShoppingCart";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from "react-bootstrap/esm/Container";
 import ListGroup from 'react-bootstrap/ListGroup';
+
 
 import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
 import Card from 'react-bootstrap/Card';
@@ -16,9 +17,11 @@ import Row from 'react-bootstrap/Row';
 import Stack from 'react-bootstrap/Stack'
 
 
+
 function ShoppingPage(prop) {
  const allItem = useOutletContext()
  const [count,setCount] = useState(0)
+ const [max,setMax] = useState(0)
   
   const [DisplayedProducts, setDisplayedProducts] = useState(
     allItem
@@ -37,14 +40,24 @@ function ShoppingPage(prop) {
         setCount(count+1)
         
       }
+
+      let handlePriceCHange = (e)=> {
+        setDisplayedProducts(allItem.filter((elem)=>{return((+elem.props.price)<=(+e.target.value))}))
+      }
       let handleSubcategory = (e)=> {
         console.log(e.target.textContent)
         if (e.target.textContent === "Sport  car") {
           setDisplayedProducts(allItem.filter((elem)=>{return(elem.props.type==="Sport")}))
-        } else if (e.target.textContent ==="Utility car") {
-          setDisplayedProducts(allItem.filter((elem)=>{return(elem.props.type==="Utility")}))
-        } else {
+        } else if (e.target.textContent ==="Suv") {
+          setDisplayedProducts(allItem.filter((elem)=>{return(elem.props.type==="Suv")}))
+        } else if (e.target.textContent ==="Coupe") {
           setDisplayedProducts(allItem.filter((elem)=>{return(elem.props.type==="Coupe")}))
+        } else if((e.target.textContent ==="Hatchbacks")) {
+          setDisplayedProducts(allItem.filter((elem)=>{return(elem.props.type==="Hatchback")}))
+        } else if((e.target.textContent ==="New")) {
+          setDisplayedProducts(allItem.filter((elem)=>{return(elem.props.status==="new")}))
+        }else if((e.target.textContent ==="Used")) {
+          setDisplayedProducts(allItem.filter((elem)=>{return(elem.props.status==="used")}))
         }
       }
       
@@ -57,6 +70,17 @@ function ShoppingPage(prop) {
       }
 
       useEffect(()=>{
+        let maxelem=  DisplayedProducts.reduce((previous,current)=>{
+          console.log(previous.props.price,current.props.price)
+          if((+previous.props.price)>(+current.props.price))
+          {
+            return previous
+          }
+          else {
+            return current
+          }})
+        console.log(maxelem)
+        setMax(maxelem.props.price)
         setShow(true);
       },[count])
 
@@ -66,20 +90,38 @@ function ShoppingPage(prop) {
     <Container fluid className="ShoppingPage "  >
       
        <Row>
-        <Col xs={2} className="sidebarwrapper">
+        <Navbar style={{display:"flex",flexDirection:"column", alignItems:"center"}}  as={Col}  xs={2} >
           
-            <Navbar  className="sidebar"sticky="top" >
-        <Nav className=" flex-column" fill justify >
+         
+        <Nav className=" flex-column" style={{position:"sticky",top:"5rem"}}  navbar fill justify >
+        <Navbar.Brand >Type:</Navbar.Brand>
         <Nav.Item onClick={handleSubcategory}><Nav.Link>Sport  car</Nav.Link></Nav.Item>
-        <Nav.Item onClick={handleSubcategory}><Nav.Link>Utility car</Nav.Link></Nav.Item>
+        <Nav.Item onClick={handleSubcategory}><Nav.Link>Suv</Nav.Link></Nav.Item>
         <Nav.Item onClick={handleSubcategory}><Nav.Link>Coupe</Nav.Link></Nav.Item>
+        <Nav.Item onClick={handleSubcategory}><Nav.Link>Hatchbacks</Nav.Link></Nav.Item>
         </Nav>
-        </Navbar>
+        <Nav className="flex-column" style={{position:"sticky",top:"18rem"}} fill justify >
+        <Navbar.Brand   >Status:</Navbar.Brand>
+        <Nav.Item onClick={handleSubcategory}><Nav.Link>New</Nav.Link></Nav.Item>
+        <Nav.Item onClick={handleSubcategory}><Nav.Link>Used</Nav.Link></Nav.Item>
+        
+        </Nav>
+       
+        <Nav className="flex-column" style={{position:"sticky",top:"24rem"}} fill justify >
+        <Navbar.Brand   >Price:</Navbar.Brand>
+        <Nav.Item >
+          <Form.Range onChange={handlePriceCHange} min={0} max={max} />
+          <Row style={{justifyContent:"space-between", flexWrap:"nowrap"}}><Container fluid>0 <div>{max}</div></Container></Row>
+        </Nav.Item>
+        
+        
+        </Nav>
+       
         
        
        
         
-       </Col>
+       </Navbar>
        <Col>
     <Row  xs={1} md={3} className="g-4">
     {DisplayedProducts.map((singleProduct,index)=>{ console.log(singleProduct)
@@ -87,12 +129,19 @@ function ShoppingPage(prop) {
         
         <Col >
            
-           <Card style={{ width: '16rem' }} id={index} >
+           <Card style={{ width: '16rem', height: "100%",}} id={index} >
        <Card.Img variant="top" style={{width:"16rem", height:"9rem"}} src={require(singleProduct.props.img+ "")}/>
       
-        <Card.Body>
-            <Card.Title>{singleProduct.props.name}</Card.Title> 
-            <Button variant="dark" id={index} onClick={addCartoCart}>Add to cart</Button>
+        <Card.Body style={{ display:"flex", flexDirection:"column" ,justifyContent:"space-between"}}>
+            <Card.Title>{singleProduct.props.name}</Card.Title>
+            <ListGroup variant="flush">
+                    <ListGroup.Item><h5>Type:</h5>{singleProduct.props.type}</ListGroup.Item>
+                    <ListGroup.Item><h5>Status:</h5>{singleProduct.props.status}</ListGroup.Item>
+                    <ListGroup.Item><h5>Km:</h5>{singleProduct.props.km}</ListGroup.Item>
+                    <ListGroup.Item><h5>Price:</h5>{singleProduct.props.price}$</ListGroup.Item>
+                   
+             </ListGroup>
+            <Button variant="dark"  id={index} onClick={addCartoCart}>Add to cart</Button>
         </Card.Body>
       
         
